a7af77a73bec2647059060dd9ff16dc5cbccd555
Vyacheslav Pukhanov <vyacheslav@pukhanov.ru>
2022-03-20 15:36:56 +0300

    add body font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,11 @@ export default function Document() {
       <Head>
         <link rel='icon' href='/favicon.ico' />
         <link
-          href='https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&display=swap'
+          href='https://fonts.googleapis.com/css2?family=Open+Sans&display=swap'
+          rel='stylesheet'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Lora:wght@400;700&display=swap'
           rel='stylesheet'
         />
         <link
